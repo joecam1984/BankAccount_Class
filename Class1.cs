803a48cf0d59f073486daf8866ea85f39ae0a661
Joseph Camacho <josephleecamachojr@outlook.com>
2023-06-26 10:19:31 -0700

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountClass
+{
+    public class Banking
+    {
+        //Fields
+        private int accountNumber;
+        private string accountHolderName;
+        private double balance;
+        private int pin;
+        //Properties
+        public int AccountNumber { get; set; }
+        public string AccountHolderName { get; set; }
+        public double Balance { get; set; }
+        public int Pin { get; set; }
+        //Constructors
+        public Banking(int accountNumber, string accountHolderName, double balance, int pin)
+        {
+            AccountNumber = accountNumber;
+            AccountHolderName = accountHolderName;
+            Balance = balance;
+            Pin = pin;
+            
+        }
+        //Methods
+        public void Withdrawal()
+        {
+            double withdrawalAmount;
+            Console.WriteLine("Withdrawal Amount");
+            withdrawalAmount = Convert.ToDouble(Console.ReadLine());
+
+            if (withdrawalAmount > Balance)
+            {
+                Console.WriteLine("Your amount exceeds your balance");
+            }
+            else
+            { 
+                Balance = Balance - withdrawalAmount;
+                Console.WriteLine("Here is your new balance: " + Balance);
+            }
+            
+        }
+        public void Deposit()
+        {
+            double depositAmount;
+            Console.WriteLine("Deposit Amount");
+            depositAmount = Convert.ToDouble(Console.ReadLine());
+
+            
+            
+                Balance = Balance + depositAmount;
+                Console.WriteLine("Here is your new balance: " + Balance);
+            
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.Design;
+
+namespace BankAccountClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Student myStudent = new Student("Peter", 25, 911, 85);
+
+            Banking newCustomer = new Banking(3698741, "Aaron", 10000.00, 1234);
+            
+            Console.WriteLine("Hello " + newCustomer.AccountHolderName + ". Your balance is: " + newCustomer.Balance);
+            Console.WriteLine("Would you like to make of deposit or withdrawal? \n Enter 'W' for withdrawal or 'D' for deposit");
+            char depOrWith = Convert.ToChar(Console.ReadLine().ToLower());
+
+            if (depOrWith.Equals('d'))
+            {
+                newCustomer.Deposit();
+            }
+            else if (depOrWith.Equals('w'))
+            {
+                newCustomer.Withdrawal();
+            }
+            else
+            {
+                Console.WriteLine("Have a wonderful day " + newCustomer.AccountHolderName);
+            }
+
+            
+            
+           
+        }
+    }
+}
